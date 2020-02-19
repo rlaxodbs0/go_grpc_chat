98ee23199842aaa83e04afb361b0dc736d4eb770
kim.ty <talzuchung@gmail.com>
2020-02-19 22:55:04 +0900

    init

--- client.go
+++ client.go
@@ -1,1 +1,67 @@
-package go_grpc_chat
+/*
+ *
+ * Copyright 2015 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+// Package main implements a client for Greeter service.
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"google.golang.org/grpc"
+	pb "go_grpc_chat/pb"
+)
+
+const (
+	address     = "localhost:50051"
+	defaultName = "world"
+)
+
+func session() {
+	var cmdString string
+	for {
+		fmt.Print("GRPCHAT >> ")
+		fmt.Scanln(&cmdString)
+		if cmdString == "exit" {
+			break
+		}
+	}
+}
+
+func main() {
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewChatTaskClient(conn)
+	name := defaultName
+	if len(os.Args) > 1 {
+		name = os.Args[1]
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.Login(ctx, &pb.UserInfo{UserName: name, Password: "ppap"})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", r.Response)
+}
--- server.go
+++ server.go
@@ -1,1 +1,49 @@
-package go_grpc_chat
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	pb "go_grpc_chat/pb"
+)
+
+const (
+	port = ":50051"
+)
+
+var user map[string]string
+
+type server struct {
+	pb.UnimplementedChatTaskServer
+}
+
+func (s *server) Login(ctx context.Context, info *pb.UserInfo) (*pb.LoginResponse, error) {
+	log.Printf("User Login: %v",info.UserName)
+	user[info.UserName] = "Active"
+	return &pb.LoginResponse{Response: pb.ResponseType_SUCCESS}, nil
+}
+
+func (s *server) Logout(ctx context.Context, info *pb.UserInfo) (*pb.LogoutResponse, error) {
+	log.Printf("User Login: %v",info.UserName)
+	delete(user, info.UserName)
+	return &pb.LogoutResponse{Response: pb.ResponseType_SUCCESS}, nil
+}
+
+func (s * server) Search(ctx context.Context, info *pb.UserInfo) (*pb.UserList, error) {
+	log.Printf("User Search: %v",info.UserName)
+	return &pb.UserList{UserNameActiveMap: user}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterChatTaskServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
